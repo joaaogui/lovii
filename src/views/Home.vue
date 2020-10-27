5be5d925c2166b31ae1b292236ee347db264350b
João Guilherme <joaaogui@gmail.com>
2020-10-26 21:53:46 -0300

    Adds logo and background

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <v-row class="logo no-gutters" >
+      <v-col>
+         <v-img  max-height="80vh" contain class="logo-image" src="@/assets/lovii_redondo.png"/>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
 
 @Component({
   components: {
@@ -16,3 +19,15 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 })
 export default class Home extends Vue {}
 </script>
+
+<style scoped lang="scss">
+.logo {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 80vh;
+  background-color: #f7a94a;
+
+}
+</style>

77a6e1fdaa788ea742ad1bae678895302c3904db
joaaogui <joaaogui@gmail.com>
2020-11-29 12:07:37 +0000

    Update gh-pages to output generated at c433a8c

---
+++ precache-manifest.1d18e61274aaa53f9c89e7f5b802efca.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98421c6f611e4184f48b",
+    "url": "css/app.b823b390.css"
+  },
+  {
+    "revision": "849739a47c8117cff433",
+    "url": "css/chunk-vendors.5785cea6.css"
+  },
+  {
+    "revision": "ae83009fb9bc8c1cfdf429cfc99f1752",
+    "url": "img/1.ae83009f.jpg"
+  },
+  {
+    "revision": "1e7b6d595bad2eb2dab3272fbbf4e7cb",
+    "url": "img/10.1e7b6d59.jpg"
+  },
+  {
+    "revision": "2bcc0f02c8fad818f523ff800dec98f0",
+    "url": "img/11.2bcc0f02.jpg"
+  },
+  {
+    "revision": "4e832b28c687e427bbd7bfd0e366e4c5",
+    "url": "img/2.4e832b28.jpg"
+  },
+  {
+    "revision": "8633b57c0e108eff277ece660a8a68e3",
+    "url": "img/3.8633b57c.jpg"
+  },
+  {
+    "revision": "660aec483d71a030e50fef622889d12a",
+    "url": "img/4.660aec48.jpg"
+  },
+  {
+    "revision": "519bbd30bb7a65693ccd8f6f88f86689",
+    "url": "img/5.519bbd30.jpg"
+  },
+  {
+    "revision": "378f854ff9a269417153cc041b949cab",
+    "url": "img/6.378f854f.jpg"
+  },
+  {
+    "revision": "e1a634e6ca79555f80c72bfd0e462ba3",
+    "url": "img/7.e1a634e6.jpg"
+  },
+  {
+    "revision": "cf0546b3228f4062dedbaba8c2026123",
+    "url": "img/8.cf0546b3.jpg"
+  },
+  {
+    "revision": "9cabdcabf8750f793d402c7edd989004",
+    "url": "img/9.9cabdcab.jpg"
+  },
+  {
+    "revision": "0fb4cff2ece5a093dba999bc553a4df3",
+    "url": "img/lovii_redondo.0fb4cff2.png"
+  },
+  {
+    "revision": "77b413ceb51e80d5f6fd0e1d8704ce60",
+    "url": "index.html"
+  },
+  {
+    "revision": "eb2cadd359d9308a3282",
+    "url": "js/about.9d18b9a4.js"
+  },
+  {
+    "revision": "98421c6f611e4184f48b",
+    "url": "js/app.17761cbf.js"
+  },
+  {
+    "revision": "849739a47c8117cff433",
+    "url": "js/chunk-vendors.43644e17.js"
+  },
+  {
+    "revision": "b18d5bfad94688f6d02450f86a74529f",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.e869e2cb514b5352f3e41d26e9328442.js"
+  "precache-manifest.1d18e61274aaa53f9c89e7f5b802efca.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "lovii-landing"});
